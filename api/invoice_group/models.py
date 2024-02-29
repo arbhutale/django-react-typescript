6a83219785fe4791cdf510bf908a89c3c508b5d6
anilbhutale <anil.bhutale@hedgehoglab.com>
2024-03-01 02:37:46 +0530

    update code

--- api/invoice_group/models.py
+++ api/invoice_group/models.py
@@ -6,8 +6,8 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 class Option(models.Model):
     name = models.CharField(max_length=100)
@@ -16,8 +16,8 @@ class Option(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.option_name
+    # def __str__(self):
+    #     return self.option_name
 
     class Meta:
-        unique_together = ('name', 'category')
+        unique_together = ('name', 'category', 'user')
